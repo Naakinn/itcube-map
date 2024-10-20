c1b501833b64a0100daa41c5da901218c4b77445
Naakinn <fedortar8@gmail.com>
2024-10-20 17:43:10 +0300

    Book name validation

--- testing/forms/book_form.py
+++ testing/forms/book_form.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 from ..models import Book
 
@@ -15,3 +16,15 @@ class BookForm(forms.ModelForm):
             "author": forms.Select(attrs={"class": "form-select"}), 
             "publish_date": forms.DateInput(attrs={"class": "form-control", "type": "date"})
         }
+    def clean_name(self): 
+        name = self.cleaned_data.get('name')
+        if re.findall(r"[^\w ]", name):
+            raise forms.ValidationError(("Invalid book name"), code="invalid")
+        return name
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        publish_date = cleaned_data.get('publish_date')
+        if publish_date and publish_date.year < 1200:
+            raise forms.ValidationError(("Books published earlier than 1200 AD are not supported"), code="invalid")
+        return cleaned_data
